80a300caaa8678f7234b43146eba8e8103cebd9e
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-07-07 11:35:12 -0400

    [gtk2-ubuntu] Fix build.sh

--- gtk2-ubuntu/build.sh
+++ gtk2-ubuntu/build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-SPECFILE=gtk2.spec
+SPECFILE=gtk2-ubuntu.spec
 MULTILIB=true
-MULTILIB_PACKAGES=('gtk2' 'gtk2-devel' 'gtk2-immodule-xim' 'gtk2-immodules')
-DO_NOT_INSTALL=('gtk2-debuginfo' 'gtk2-devel-docs')
+MULTILIB_PACKAGES=('gtk2-ubuntu' 'gtk2-ubuntu-devel' 'gtk2-ubuntu-immodule-xim'
+                   'gtk2-ubuntu-immodules')
+DO_NOT_INSTALL=('gtk2-ubuntu-debuginfo' 'gtk2-ubuntu-devel-docs')
 
 source "$(dirname ${0})/../internal_builder.sh"
 build ${1}
