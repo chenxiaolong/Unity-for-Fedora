60855ad256e9a8bff78f34ae867860b688214376
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-07-07 11:36:42 -0400

    [gtk3-ubuntu] Fix build.sh

--- gtk3-ubuntu/build.sh
+++ gtk3-ubuntu/build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-SPECFILE=gtk3.spec
+SPECFILE=gtk3-ubuntu.spec
 MULTILIB=true
 # gtk3-immodule-xim and gtk3-immodules are not multilib in Fedora for some
 # reason (gtk2 versions are).
-MULTILIB_PACKAGES=('gtk3' 'gtk3-devel')
-DO_NOT_INSTALL=('gtk3-debuginfo' 'gtk3-devel-docs')
+MULTILIB_PACKAGES=('gtk3-ubuntu' 'gtk3-ubuntu-devel')
+DO_NOT_INSTALL=('gtk3-ubuntu-debuginfo' 'gtk3-ubuntu-devel-docs')
 # Same Fedora maintainer as glib2; doesn't care about non-multilib devel
 # packages.
-MULTILIB_DO_NOT_INSTALL=('gtk3-devel')
+MULTILIB_DO_NOT_INSTALL=('gtk3-ubuntu-devel')
 
 source "$(dirname ${0})/../internal_builder.sh"
 build ${1}
