fff259c1901919540f1e5d2533995cad64c189cc
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-07-08 13:19:39 -0400

    [gtk3-ubuntu] Fix version comparison script

--- gtk3-ubuntu/compare_versions.sh
+++ gtk3-ubuntu/compare_versions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-SPEC_VER="$(rpmspec -q --qf '%{version}\n' gtk3.spec | head -1)"
-UBUNTU_REL="$(sed -n 's/^%define[ ]*_ubuntu_rel[ ]*\(.*\)$/\1/p' gtk3.spec)"
+SPEC_VER="$(rpmspec -q --qf '%{version}\n' gtk3-ubuntu.spec | head -1)"
+UBUNTU_REL="$(sed -n 's/^%define[ ]*_ubuntu_rel[ ]*\(.*\)$/\1/p' gtk3-ubuntu.spec)"
 
 echo "Getting latest Ubuntu version..."
 UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise-updates/source/gtk+3.0' -O - | sed -n 's/.*>gtk+3.0_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
