b2bba4953a8f002dd96ab3295cf1898b80de3681
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-07-24 12:09:17 -0700

    [compizconfig-backend-gconf] Deleted: Obsoleted by compiz

--- compizconfig-backend-gconf/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SPECFILE=compizconfig-backend-gconf.spec
-MULTILIB=false
-DO_NOT_INSTALL=('compizconfig-backend-gconf-debuginfo')
-
-source "$(dirname ${0})/../internal_builder.sh"
-build ${1}
