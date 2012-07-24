106ce4cb15c3597725761f807a673e9c0b2441e1
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-07-24 12:10:43 -0700

    [python-compizconfig] Deleted: Obsoleted by compiz

--- python-compizconfig/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SPECFILE=python-compizconfig.spec
-MULTILIB=false
-DO_NOT_INSTALL=('python-compizconfig-debuginfo')
-
-source "$(dirname ${0})/../internal_builder.sh"
-build ${1}
