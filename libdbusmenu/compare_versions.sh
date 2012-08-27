dd11956d4965d62ce267ce2e47b6743e5e3fb3f2
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-08-27 17:47:38 -0400

    [libdbusmenu] Fix version comparison script

--- libdbusmenu/compare_versions.sh
+++ libdbusmenu/compare_versions.sh
@@ -4,7 +4,7 @@ SPEC_VER="$(rpmspec -q --qf '%{version}\n' libdbusmenu.spec | head -1)"
 UBUNTU_REL="$(sed -n 's/^%define[ ]*_ubuntu_rel[ ]*\(.*\)$/\1/p' libdbusmenu.spec)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/libdbusmenu' -O - | sed -n 's/.*>libdbusmenu_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libdbusmenu' -O - | sed -n 's/.*>libdbusmenu_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/dbusmenu/+download' -O - | sed -n 's/.*libdbusmenu-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
