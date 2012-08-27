a72e85c73768e9651c396cc5fc4f4e7ab352cf3d
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-08-27 18:08:08 -0400

    [libgdu] Dropped

--- libgdu/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SPECFILE=libgdu.spec
-MULTILIB=true
-MULTILIB_PACKAGES=('libgdu' 'libgdu-devel')
-DO_NOT_INSTALL=('libgdu-debuginfo')
-
-source "$(dirname ${0})/../internal_builder.sh"
-build ${1}
