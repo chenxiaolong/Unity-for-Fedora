4773057fd90b88835894a39eb84432fb928727f8
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-08-27 18:03:35 -0400

    [gnome-settings-daemon-ubuntu] Fix version comparison script

--- gnome-settings-daemon-ubuntu/compare_versions.sh
+++ gnome-settings-daemon-ubuntu/compare_versions.sh
@@ -4,7 +4,7 @@ SPEC_VER="$(rpmspec -q --qf '%{version}\n' gnome-settings-daemon-ubuntu.spec | h
 UBUNTU_REL="$(sed -n 's/^%define[ ]*_ubuntu_rel[ ]*\(.*\)$/\1/p' gnome-settings-daemon-ubuntu.spec)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise-updates/source/gnome-settings-daemon' -O - | sed -n 's/.*>gnome-settings-daemon_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/gnome-settings-daemon' -O - | sed -n 's/.*>gnome-settings-daemon_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/gnome-settings-daemon/3.4/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
