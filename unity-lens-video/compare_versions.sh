eca3a87163883b7a206f407946639ba4103c865d
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-08-28 00:39:50 -0400

    [unity-lens-video] Fix version comparison script

--- unity-lens-video/compare_versions.sh
+++ unity-lens-video/compare_versions.sh
@@ -3,7 +3,7 @@
 SPEC_VER="$(rpmspec -q --qf '%{version}\n' unity-lens-video.spec | head -1)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise-updates/source/unity-lens-video' -O - | sed -n 's/.*>unity-lens-video_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/unity-lens-video' -O - | sed -n 's/.*>unity-lens-video_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/unity-lens-videos/+download' -O - | sed -n 's/.*unity-lens-video-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
