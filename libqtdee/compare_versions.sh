56ce84d4531f1a3906ba7c4c67777abc014c98ea
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-03 13:40:30 -0400

    [libqtdee] Update version comparison script

--- libqtdee/compare_versions.sh
+++ libqtdee/compare_versions.sh
@@ -3,7 +3,7 @@
 SPEC_VER="$(rpmspec -q --qf '%{version}\n' libqtdee.spec | head -1)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/libqtdee' -O - | sed -n 's/.*>libqtdee_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libqtdee' -O - | sed -n 's/.*>libqtdee_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo ""
 
