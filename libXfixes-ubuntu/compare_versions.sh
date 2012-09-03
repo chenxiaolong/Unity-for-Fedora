8758a2ee8f7a874245e76ebc696dbfab5ee21626
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-03 13:56:09 -0400

    [libXfixes-ubuntu] Update version comparison script

--- libXfixes-ubuntu/compare_versions.sh
+++ libXfixes-ubuntu/compare_versions.sh
@@ -4,7 +4,7 @@ SPEC_VER="$(rpmspec -q --qf '%{version}\n' libXfixes-ubuntu.spec | head -1)"
 UBUNTU_REL="$(sed -n 's/^%define[ ]*_ubuntu_rel[ ]*\(.*\)$/\1/p' libXfixes-ubuntu.spec)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/libxfixes' -O - | sed -n 's/.*>libxfixes_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libxfixes' -O - | sed -n 's/.*>libxfixes_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q http://xorg.freedesktop.org/releases/individual/lib/ -O - | sed -n 's/.*libXfixes-\(.*\).tar.bz2.*/\1/p' | tail -n 1)
