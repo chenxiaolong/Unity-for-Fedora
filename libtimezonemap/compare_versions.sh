044a15caef9ac816b4573dbc757d636fda685f30
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-03 13:47:13 -0400

    [libtimezonemap] Update version comparison script

--- libtimezonemap/compare_versions.sh
+++ libtimezonemap/compare_versions.sh
@@ -3,7 +3,7 @@
 SPEC_VER="$(rpmspec -q --qf '%{version}\n' libtimezonemap.spec | head -1)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/libtimezonemap' -O - | sed -n 's/.*>libtimezonemap_\(.*\)\.tar\.gz<.*/\1/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libtimezonemap' -O - | sed -n 's/.*>libtimezonemap_\(.*\)\.tar\.gz<.*/\1/p'))
 
 echo ""
 
