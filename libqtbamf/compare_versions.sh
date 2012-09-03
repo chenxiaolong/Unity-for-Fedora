95a5b8e48518a64b59a7df884ee9c82b7db96236
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-03 13:40:06 -0400

    [libqtbamf] Update version comparison script

--- libqtbamf/compare_versions.sh
+++ libqtbamf/compare_versions.sh
@@ -3,7 +3,7 @@
 SPEC_VER="$(rpmspec -q --qf '%{version}\n' libqtbamf.spec | head -1)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise-updates/source/libqtbamf' -O - | sed -n 's/.*>libqtbamf_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libqtbamf' -O - | sed -n 's/.*>libqtbamf_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
 
 echo ""
 
