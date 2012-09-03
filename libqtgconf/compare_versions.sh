db78adcb5dd15eb7eff4793b2eaf4ca401bde386
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-03 13:40:55 -0400

    [libqtgconf] Update version comparison script

--- libqtgconf/compare_versions.sh
+++ libqtgconf/compare_versions.sh
@@ -3,7 +3,7 @@
 SPEC_VER="$(rpmspec -q --qf '%{version}\n' libqtgconf.spec | head -1)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/libqtgconf' -O - | sed -n 's/.*>libqtgconf_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libqtgconf' -O - | sed -n 's/.*>libqtgconf_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
 
 echo ""
 
