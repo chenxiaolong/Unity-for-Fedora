434565a136b92975bc1e84f40ba5b572407ffd70
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-03 13:34:08 -0400

    [gtk2-ubuntu] Fix version comparison script

--- gtk2-ubuntu/compare_versions.sh
+++ gtk2-ubuntu/compare_versions.sh
@@ -4,7 +4,7 @@ SPEC_VER="$(rpmspec -q --qf '%{version}\n' gtk2-ubuntu.spec | head -1)"
 UBUNTU_REL="$(sed -n 's/^%define[ ]*_ubuntu_rel[ ]*\(.*\)$/\1/p' gtk2-ubuntu.spec)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/gtk+2.0' -O - | sed -n 's/.*>gtk+2.0_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/gtk+2.0' -O - | sed -n 's/.*>gtk+2.0_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/gtk+/${SPEC_VER%.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
