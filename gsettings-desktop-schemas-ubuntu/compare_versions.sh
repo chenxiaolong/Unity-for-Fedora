29db629f0ef8b07ee050ace69ceb0524d3d9f868
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-20 21:37:31 -0400

    [gsettings-desktop-schemas-ubuntu] Update version comparison script

--- gsettings-desktop-schemas-ubuntu/compare_versions.sh
+++ gsettings-desktop-schemas-ubuntu/compare_versions.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-SPEC_VER="$(rpmspec -q --qf '%{version}\n' gsettings-desktop-schemas-ubuntu.spec | head -1)"
-UBUNTU_REL="$(sed -n 's/^%define[ ]*_ubuntu_rel[ ]*\(.*\)$/\1/p' gsettings-desktop-schemas-ubuntu.spec)"
+F17_SPEC_VER="$(rpmspec -q --qf '%{version}\n' gsettings-desktop-schemas-ubuntu-Fedora_17.spec | head -1)"
+F18_SPEC_VER="$(rpmspec -q --qf '%{version}\n' gsettings-desktop-schemas-ubuntu-Fedora_18.spec | head -1)"
+F17_UBUNTU_REL="$(sed -n 's/^%define[ ]*_ubuntu_rel[ ]*\(.*\)$/\1/p' gsettings-desktop-schemas-ubuntu-Fedora_17.spec)"
+F18_UBUNTU_REL="$(sed -n 's/^%define[ ]*_ubuntu_rel[ ]*\(.*\)$/\1/p' gsettings-desktop-schemas-ubuntu-Fedora_18.spec)"
 
 echo "Getting latest Ubuntu version..."
 UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/gsettings-desktop-schemas' -O - | sed -n 's/.*>gsettings-desktop-schemas_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/gsettings-desktop-schemas/3.4/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
+UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/gsettings-desktop-schemas/3.5/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
 
 echo ""
 
-echo -e "spec file version: ${SPEC_VER} ${UBUNTU_REL}"
-echo -e "Upstream version:  ${UPSTREAM_VER}"
-echo -e "Ubuntu version:    ${UBUNTU_VER[@]}"
+echo -e "F17 spec version: ${F17_SPEC_VER} ${F17_UBUNTU_REL}"
+echo -e "F18 spec version: ${F18_SPEC_VER} ${F18_UBUNTU_REL}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
