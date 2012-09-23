91846b4595703edfe46d317252f2d7b925c32ea8
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-23 16:52:01 -0400

    [appmenu-qt] Update version comparison script

--- appmenu-qt/compare_versions.sh
+++ appmenu-qt/compare_versions.sh
@@ -3,7 +3,7 @@
 SPEC_VER="$(rpmspec -q --qf '%{version}\n' appmenu-qt.spec | head -1)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/appmenu-qt' -O - | sed -n 's/.*>appmenu-qt_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/appmenu-qt' -O - | sed -n 's/.*>appmenu-qt_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/appmenu-qt/+download' -O - | sed -n 's/.*appmenu-qt-\(.*\)\.tar\.bz2.*/\1/p' | head -n 1)
