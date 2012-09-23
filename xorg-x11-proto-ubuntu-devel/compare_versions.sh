43133fd7557df246057baab38e0787133c4b642a
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-23 16:45:45 -0400

    [xorg-x11-proto-ubuntu-devel] Update version comparison script

--- xorg-x11-proto-ubuntu-devel/compare_versions.sh
+++ xorg-x11-proto-ubuntu-devel/compare_versions.sh
@@ -10,7 +10,7 @@ F18_SPEC_REAL_VER="$(rpmspec -q --qf '%{version}\n' xorg-x11-proto-ubuntu-devel-
 F18_SPEC_FCREL="$(sed -n 's/^%define[ \t]*_fedora_rel[ \t]*\(.*\)$/\1/p' xorg-x11-proto-ubuntu-devel-Fedora_18.spec)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/x11proto-fixes' -O - | sed -n 's/.*>x11proto-fixes_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/x11proto-fixes' -O - | sed -n 's/.*>x11proto-fixes_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest Fedora version..."
 FEDORA_VER="$(wget -q 'http://pkgs.fedoraproject.org/gitweb/?p=xorg-x11-proto-devel.git;a=blob_plain;f=xorg-x11-proto-devel.spec;hb=HEAD' -O tmp.spec && rpm -q --qf '%{version} %{release}' --specfile tmp.spec | sed 's/\.fc[0-9]*//' && rm tmp.spec)"
