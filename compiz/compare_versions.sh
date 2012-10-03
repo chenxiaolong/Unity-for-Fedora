78acd9d5e4eaeeb0301aeb0fe7e6626cf9eeedb7
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-03 14:59:07 -0400

    [compiz] Update version comparison script

--- compiz/compare_versions.sh
+++ compiz/compare_versions.sh
@@ -2,13 +2,12 @@
 
 SPEC_VER="$(rpmspec -q --qf '%{version}\n' compiz.spec | head -1)"
 UBUNTU_REL="$(sed -n 's/^%define[ ]*_ubuntu_rel[ ]*\(.*\)$/\1/p' compiz.spec)"
-BZR_REV="$(sed -n 's/^%define[ ]*_bzr_rev[ ]*\(.*\)$/\1/p' compiz.spec)"
 
 echo "Getting latest Ubuntu version..."
 UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/compiz' -O - | sed -n 's/.*>compiz_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo ""
 
-echo -e "spec file version: ${SPEC_VER}+bzr${BZR_REV} ${UBUNTU_REL}"
+echo -e "spec file version: ${SPEC_VER} ${UBUNTU_REL}"
 echo -e "Upstream version:  (none)"
 echo -e "Ubuntu version:    ${UBUNTU_VER[@]}"
