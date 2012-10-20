e254c04542a9e80d8e3ed184a82b18615b50ccd9
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-20 16:58:28 -0400

    [python-oauthlib] Version comparison script: Parse Launchpad webpage to get Ubuntu version

--- python-oauthlib/compare_versions.sh
+++ python-oauthlib/compare_versions.sh
@@ -4,7 +4,7 @@ SPEC_VER="$(rpmspec -q --qf '%{version}\n' python-oauthlib.spec | head -1)"
 UBUNTU_REL="$(sed -n 's/^%define[ ]*_ubuntu_rel[ ]*\(.*\)$/\1/p' python-oauthlib.spec)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/python-oauthlib' -O - | sed -n 's/.*>python-oauthlib_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/python-oauthlib' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'http://pypi.python.org/pypi/oauthlib' -O - | sed -n 's/.*>oauthlib-\(.*\)\.tar\.gz<.*/\1/p' | head -1)
