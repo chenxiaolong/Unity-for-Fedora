cf086d3b7b4653cea4b43c5a99aaf7154ac5c5e7
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-20 16:04:00 -0400

    [indicator-printers] Version comparison script: Parse Launchpad webpage to get Ubuntu version

--- indicator-printers/compare_versions.sh
+++ indicator-printers/compare_versions.sh
@@ -4,7 +4,7 @@ SPEC_VER="$(rpmspec -q --qf '%{version}\n' indicator-printers.spec | head -1)"
 UBUNTU_REL="$(sed -n 's/^%define[ ]*_ubuntu_rel[ ]*\(.*\)$/\1/p' indicator-printers.spec)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/indicator-printers' -O - | sed -n 's/.*>indicator-printers_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/indicator-printers' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/indicator-printers/+download' -O - | sed -n 's/.*indicator-printers-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
