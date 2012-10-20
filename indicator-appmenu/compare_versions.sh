ddfdc4a99a95e70132f3e7c4da1365d8c59f83a5
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-20 13:59:06 -0400

    [indicator-appmenu] Version comparison script: Parse Launchpad webpage to get Ubuntu version

--- indicator-appmenu/compare_versions.sh
+++ indicator-appmenu/compare_versions.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-SPEC_VER="$(rpmspec -q --qf '%{version}\n' indicator-appmenu.spec | head -1)"
+F17_SPEC_VER="$(rpmspec -q --qf '%{version}\n' indicator-appmenu-Fedora_17.spec | head -1)"
+F18_SPEC_VER="$(rpmspec -q --qf '%{version}\n' indicator-appmenu-Fedora_18.spec | head -1)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/indicator-appmenu' -O - | sed -n 's/.*>indicator-appmenu_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/indicator-appmenu' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/indicator-appmenu/+download' -O - | sed -n 's/.*indicator-appmenu-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
 
 echo ""
 
-echo -e "spec file version: ${SPEC_VER}"
-echo -e "Upstream version:  ${UPSTREAM_VER}"
-echo -e "Ubuntu version:    ${UBUNTU_VER[@]}"
+echo -e "F17 spec version: ${F17_SPEC_VER}"
+echo -e "F18 spec version: ${F18_SPEC_VER}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
