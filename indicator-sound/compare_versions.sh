30632d0c5340bd29466a6161a89a5df479a1b786
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-20 16:07:15 -0400

    [indicator-sound] Version comparison script: Parse Launchpad webpage to get Ubuntu version

--- indicator-sound/compare_versions.sh
+++ indicator-sound/compare_versions.sh
@@ -4,7 +4,7 @@ SPEC_VER="$(rpmspec -q --qf '%{version}\n' indicator-sound.spec | head -1)"
 UBUNTU_REL="$(sed -n 's/^%define[ ]*_ubuntu_rel[ ]*\(.*\)$/\1/p' indicator-sound.spec)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/indicator-sound' -O - | sed -n 's/.*>indicator-sound_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/indicator-sound' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/indicator-sound/+download' -O - | sed -n 's/.*indicator-sound-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
