a334ef42c4083e63a6ec70aa95cdbe20c5252f30
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-30 01:12:13 -0400

    [libunity-webapps] Get Ubuntu version from Launchpad

--- libunity-webapps/compare_versions.sh
+++ libunity-webapps/compare_versions.sh
@@ -4,7 +4,7 @@ F17_SPEC_VER="$(rpmspec -q --qf '%{version}\n' libunity-webapps-Fedora_17.spec |
 F18_SPEC_VER="$(rpmspec -q --qf '%{version}\n' libunity-webapps-Fedora_18.spec | head -1)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libunity-webapps' -O - | sed -n 's/.*>libunity-webapps_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/libunity-webapps' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/libunity-webapps/+download' -O - | sed -n 's/.*unity_webapps-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
