806ca9f943bcc4e1d0249351e32bb8e7a1d84935
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-30 01:20:19 -0400

    [libaccounts-glib] Get Ubuntu version from Launchpad

--- libaccounts-glib/compare_versions.sh
+++ libaccounts-glib/compare_versions.sh
@@ -4,7 +4,7 @@ SPEC_VER="$(rpmspec -q --qf '%{version}\n' libaccounts-glib.spec | head -1)"
 UBUNTU_REL="$(sed -n 's/^%define[ ]*_ubuntu_rel[ ]*\(.*\)$/\1/p' libaccounts-glib.spec)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libaccounts-glib' -O - | sed -n 's/.*>libaccounts-glib_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/libaccounts-glib' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q "https://code.google.com/p/accounts-sso/downloads/list" -O - | sed -n 's/.*libaccounts-glib-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
