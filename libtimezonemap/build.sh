b229697b82758c17ee746d849040b688d1ba13cf
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-30 01:41:18 -0400

    [libtimezonemap] Removed: not needed anymore

--- libtimezonemap/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SPECFILE=libtimezonemap.spec
-MULTILIB=true
-MULTILIB_PACKAGES=('libtimezonemap' 'libtimezonemap-devel')
-DO_NOT_INSTALL=('libtimezonemap-debuginfo')
-
-source "$(dirname ${0})/../internal_builder.sh"
-build ${1}
