e5b39547dac8207fb4012140fec3c78090ff705d
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-30 01:11:37 -0400

    [indicator-messages] Get Ubuntu version from Launchpad

--- indicator-messages/compare_versions.sh
+++ indicator-messages/compare_versions.sh
@@ -4,7 +4,7 @@ F17_SPEC_VER="$(rpmspec -q --qf '%{version}\n' indicator-messages-Fedora_17.spec
 F18_SPEC_VER="$(rpmspec -q --qf '%{version}\n' indicator-messages-Fedora_18.spec | head -1)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/indicator-messages' -O - | sed -n 's/.*>indicator-messages_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/indicator-messages' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/indicator-messages/+download' -O - | sed -n 's/.*indicator-messages-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
