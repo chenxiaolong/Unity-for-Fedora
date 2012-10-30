bf30726fcc7fc99d27f4c895591e4872ec13c357
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-30 01:17:46 -0400

    [libqtgconf] Get Ubuntu version from Launchpad

--- libqtgconf/compare_versions.sh
+++ libqtgconf/compare_versions.sh
@@ -3,7 +3,7 @@
 SPEC_VER="$(rpmspec -q --qf '%{version}\n' libqtgconf.spec | head -1)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libqtgconf' -O - | sed -n 's/.*>libqtgconf_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/libqtgconf' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo ""
 
