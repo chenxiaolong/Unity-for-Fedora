014cb1cfe8b0dfa5107ff97e9b301a1f4a41cf9a
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-31 20:45:59 -0400

    [libqtgconf] Removed: only needed by the deprecated Unity 2D

--- libqtgconf/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SPECFILE=libqtgconf.spec
-MULTILIB=true
-MULTILIB_PACKAGES=('libqtgconf' 'libqtgconf-devel')
-DO_NOT_INSTALL=('libqtgconf-debuginfo')
-
-source "$(dirname ${0})/../internal_builder.sh"
-build ${1}
