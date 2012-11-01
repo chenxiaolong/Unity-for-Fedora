bec97600e559bf8f6088f60497d5367b047dc5d2
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-31 20:38:16 -0400

    [libqtdee] Dropped: only needed by the deprecated Unity 2D

--- libqtdee/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SPECFILE=libqtdee.spec
-MULTILIB=true
-MULTILIB_PACKAGES=('libqtdee' 'libqtdee-devel')
-DO_NOT_INSTALL=('libqtdee-debuginfo')
-
-source "$(dirname ${0})/../internal_builder.sh"
-build ${1}
