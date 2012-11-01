fce15a9e7e6ab7542e615f7845b665bff46e5484
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-31 20:54:28 -0400

    [dconf-qt] Removed: only used by the deprecated Unity 2D

--- dconf-qt/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SPECFILE=dconf-qt.spec
-MULTILIB=true
-MULTILIB_PACKAGES=('dconf-qt' 'dconf-qt-devel')
-DO_NOT_INSTALL=('dconf-qt-debuginfo')
-
-source "$(dirname ${0})/../internal_builder.sh"
-build ${1}
