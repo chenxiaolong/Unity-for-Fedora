eaf370d22e631533f60a15bfdf472a3da0eff148
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-11-03 03:14:01 -0400

    [libqtbamf] Removed: it is only used by the deprecated Unity 2D

--- libqtbamf/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SPECFILE=libqtbamf.spec
-MULTILIB=true
-MULTILIB_PACKAGES=('bamf-qt' 'bamf-qt-devel')
-DO_NOT_INSTALL=('bamf-qt-debuginfo')
-
-source "$(dirname ${0})/../internal_builder.sh"
-build ${1}
