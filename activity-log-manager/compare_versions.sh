dab2ddc596d945c54b750fbd7c9592b0cabeeabf
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-11-26 15:19:33 -0500

    [activity-log-manager] Update version comparison script

--- activity-log-manager/compare_versions.sh
+++ activity-log-manager/compare_versions.sh
@@ -4,7 +4,7 @@ SPEC_VER="$(rpmspec -q --qf '%{version}\n' activity-log-manager.spec | head -1)"
 UBUNTU_REL="$(sed -n 's/^%define[ ]*_ubuntu_rel[ ]*\(.*\)$/\1/p' activity-log-manager.spec)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/activity-log-manager' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/raring/+source/activity-log-manager' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/activity-log-manager/+download' -O - | sed -n 's/.*activity-log-manager-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
