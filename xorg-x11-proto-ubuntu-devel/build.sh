f6c432170da1f781242680385b1837bba16e7a95
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-11-26 22:53:11 -0500

    [xorg-x11-proto-ubuntu-devel] Update build script

--- xorg-x11-proto-ubuntu-devel/build.sh
+++ xorg-x11-proto-ubuntu-devel/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SPECFILE=xorg-x11-proto-ubuntu-devel.spec
+SPECFILE=xorg-x11-proto-ubuntu-devel-Fedora_$(rpm -E '%fedora').spec
 MULTILIB=false
 
 source "$(dirname ${0})/../internal_builder.sh"
