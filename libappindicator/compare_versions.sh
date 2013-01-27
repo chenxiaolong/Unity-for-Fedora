804a9c4dea8a0ecd0652b816412c383743feb971
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-27 18:29:15 -0500

    [libappindicator] Use new version comparison script

--- libappindicator/compare_versions.sh
+++ libappindicator/compare_versions.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-SPEC_VER="$(rpmspec -q --qf '%{version}\n' libappindicator.spec | head -1)"
+SPECFILE=libappindicator.spec
 
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/libappindicator' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
+source "$(dirname ${0})/../version_checker.sh"
 
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'https://launchpad.net/libappindicator/+download' -O - | sed -n 's/.*libappindicator-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
-
-echo ""
-
-echo -e "spec file version: ${SPEC_VER}"
-echo -e "Upstream version:  ${UPSTREAM_VER}"
-echo -e "Ubuntu version:    ${UBUNTU_VER[@]}"
+echo -e "spec file version: $(get_spec_version)"
+echo -e "Upstream version:  $(get_launchpad_version libappindicator)"
+echo -e "Ubuntu version:    $(get_ubuntu_version libappindicator ${1:-raring})"
