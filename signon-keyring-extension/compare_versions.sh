069abde3d388573ae745ffa4ca156983604847f3
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-28 14:35:50 -0500

    [signon-keyring-extension] Update to version 0.4daily12.12.06

--- signon-keyring-extension/compare_versions.sh
+++ signon-keyring-extension/compare_versions.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-SPEC_VER="$(rpmspec -q --qf '%{version}\n' signon-keyring-extension.spec | head -1)"
+SPECFILE=signon-keyring-extension.spec
 
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/signon-keyring-extension' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
+source "$(dirname ${0})/../version_checker.sh"
 
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'https://launchpad.net/online-accounts-keyring-extension/+download' -O - | sed -n 's/.*keyring-\(.*\)\.tar\.bz2.*/\1/p' | head -n 1)
-
-echo ""
-
-echo -e "spec file version: ${SPEC_VER} ${UBUNTU_REL}"
-echo -e "Upstream version:  ${UPSTREAM_VER}"
-echo -e "Ubuntu version:    ${UBUNTU_VER[@]}"
+echo -e "spec file version: $(get_spec_version)"
+echo -e "Upstream version:  $(get_launchpad_version online-accounts-keyring-extension keyring)"
+echo -e "Ubuntu version:    $(get_ubuntu_version signon-keyring-extension ${1:-raring})"
--- signon-keyring-extension/signon-keyring-extension.spec
+++ signon-keyring-extension/signon-keyring-extension.spec
@@ -8,14 +8,14 @@
 # We'll use the same name as Ubuntu (signon-keyring-extension)
 
 Name:		signon-keyring-extension
-Version:	0.4
+Version:	0.4daily12.12.06
 Release:	1%{?dist}
 Summary:	GNOME Keyring Extension for signond
 
 Group:		User Interface/Desktops
 License:	LGPLv2
 URL:		https://launchpad.net/online-accounts-keyring-extension
-Source0:	https://launchpad.net/online-accounts-keyring-extension/trunk/%{version}/+download/keyring-%{version}.tar.bz2
+Source0:	https://launchpad.net/ubuntu/+archive/primary/+files/signon-keyring-extension_%{version}.orig.tar.gz
 
 BuildRequires:	pkgconfig
 
@@ -29,7 +29,7 @@ GNOME Keyring.
 
 
 %prep
-%setup -q -n keyring-%{version}
+%setup -q
 
 
 %build
@@ -55,6 +55,9 @@ rm -f $RPM_BUILD_ROOT%{_bindir}/keyring-test
 
 
 %changelog
+* Mon Jan 27 2013 Xiao-Long Chen <chenxiaolong@cxl.epac.to> - 0.4daily12.12.06-1
+- Version 0.4daily12.12.06
+
 * Tue Sep 04 2012 Xiao-Long Chen <chenxiaolong@cxl.epac.to> - 0.4-1
 - Initial release
 - Version 0.4
