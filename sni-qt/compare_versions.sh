166eaceb677af04c18e92e90613c4141a9708b0f
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-27 20:34:42 -0500

    [sni-qt] Use new version comparison script

--- sni-qt/compare_versions.sh
+++ sni-qt/compare_versions.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-SPEC_VER="$(rpmspec -q --qf '%{version}\n' sni-qt.spec | head -1)"
+SPECFILE=sni-qt.spec
 
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/sni-qt' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
+source "$(dirname ${0})/../version_checker.sh"
 
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'https://launchpad.net/sni-qt/+download' -O - | sed -n 's/.*sni-qt-\(.*\)\.tar\.bz2.*/\1/p' | head -n 1)
-
-echo ""
-
-echo -e "spec file version: ${SPEC_VER}"
-echo -e "Upstream version:  ${UPSTREAM_VER}"
-echo -e "Ubuntu version:    ${UBUNTU_VER[@]}"
+echo -e "spec file version: $(get_spec_version)"
+echo -e "Upstream version:  $(get_launchpad_version sni-qt)"
+echo -e "Ubuntu version:    $(get_ubuntu_version sni-qt ${1:-raring})"
