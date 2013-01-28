5387c9bc1d9d3c0d97b14449848aca6bb86f104e
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-27 19:18:13 -0500

    [libunity-misc] Use new version comparison script

--- libunity-misc/compare_versions.sh
+++ libunity-misc/compare_versions.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
 
-SPEC_VER="$(rpmspec -q --qf '%{version}\n' libunity-misc.spec | head -1)"
-UBUNTU_REL="$(sed -n 's/^%define[ ]*_ubuntu_rel[ ]*\(.*\)$/\1/p' libunity-misc.spec)"
+SPECFILE=libunity-misc.spec
 
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/libunity-misc' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
+source "$(dirname ${0})/../version_checker.sh"
 
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'https://launchpad.net/libunity-misc/+download' -O - | sed -n 's/.*libunity-misc-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
-
-echo ""
-
-echo -e "spec file version: ${SPEC_VER} ${UBUNTU_REL}"
-echo -e "Upstream version:  ${UPSTREAM_VER}"
-echo -e "Ubuntu version:    ${UBUNTU_VER[@]}"
+echo -e "spec file version: $(get_spec_version) $(get_spec_release --ubuntu)"
+echo -e "Upstream version:  $(get_launchpad_version libunity-misc)"
+echo -e "Ubuntu version:    $(get_ubuntu_version libunity-misc ${1:-raring})"
