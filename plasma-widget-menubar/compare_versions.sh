ab6a45fbf5365f9c145c57c68aef5f77a47ce5f4
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-29 13:22:23 -0500

    [plasma-widget-menubar] Use new version comparison script

--- plasma-widget-menubar/compare_versions.sh
+++ plasma-widget-menubar/compare_versions.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-SPEC_VER="$(rpmspec -q --qf '%{version}\n' plasma-widget-menubar.spec | head -1)"
+SPECFILE=plasma-widget-menubar.spec
 
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/plasma-widget-menubar' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
+source "$(dirname ${0})/../version_checker.sh"
 
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'https://launchpad.net/plasma-widget-menubar/+download' -O - | sed -n 's/.*plasma-widget-menubar-\(.*\)\.tar\.bz2.*/\1/p' | head -n 1)
-
-echo ""
-
-echo -e "spec file version: ${SPEC_VER} ${UBUNTU_REL}"
-echo -e "Upstream version:  ${UPSTREAM_VER}"
-echo -e "Ubuntu version:    ${UBUNTU_VER[@]}"
+echo -e "spec file version: $(get_spec_version)"
+echo -e "Upstream version:  $(get_launchpad_version plasma-widget-menubar)"
+echo -e "Ubuntu version:    $(get_ubuntu_version plasma-widget-menubar ${1:-raring})"
