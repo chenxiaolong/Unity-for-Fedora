249b10f922fe07707452c9cd97c4b17cfd74910f
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-30 17:13:34 -0500

    [nux] Use new version comparison script

--- nux/compare_versions.sh
+++ nux/compare_versions.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
-SPEC_VER="$(rpmspec -q --qf '%{version}\n' nux.spec | head -1)"
+source "$(dirname ${0})/../version_checker.sh"
 
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/nux' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
-
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'https://launchpad.net/nux/+download' -O - | sed -n 's/.*nux-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
-
-echo ""
-
-echo -e "spec file version: ${SPEC_VER}"
-echo -e "Upstream version:  ${UPSTREAM_VER}"
-echo -e "Ubuntu version:    ${UBUNTU_VER[@]}"
+SPECFILE=nux-Fedora_17.spec
+echo -e "F17 spec version: $(get_spec_version)"
+SPECFILE=nux-Fedora_18.spec
+echo -e "F18 spec version: $(get_spec_version)"
+echo -e "Upstream version: $(get_launchpad_version nux)"
+echo -e "Ubuntu version:   $(get_ubuntu_version nux ${1:-raring})"
