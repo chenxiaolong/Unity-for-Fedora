8281a38411c9798e5acae7dbacd15137d6b317da
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-31 20:29:16 -0500

    [python3-distutils-extra] Use new version comparison script

--- python3-distutils-extra/compare_versions.sh
+++ python3-distutils-extra/compare_versions.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-SPEC_VER="$(rpmspec -q --qf '%{version}\n' python3-distutils-extra.spec | head -1)"
+SPECFILE=python3-distutils-extra.spec
 
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/python-distutils-extra' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
+source "$(dirname ${0})/../version_checker.sh"
 
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'https://launchpad.net/python-distutils-extra/+download' -O - | sed -n 's/.*python-distutils-extra-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
-
-echo ""
-
-echo -e "spec file version: ${SPEC_VER}"
-echo -e "Upstream version:  ${UPSTREAM_VER}"
-echo -e "Ubuntu version:    ${UBUNTU_VER[@]}"
+echo -e "spec file version: $(get_spec_version)"
+echo -e "Upstream version:  $(get_launchpad_version python-distutils-extra)"
+echo -e "Ubuntu version:    $(get_ubuntu_version python-distutils-extra ${1:-raring})"
