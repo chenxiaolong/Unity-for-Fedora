7265d06d8f4d9f1e3fa751f261894cf22b5797d5
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-02-01 13:22:50 -0500

    [NetworkManager-gnome-ubuntu] Use new version comparison script

--- NetworkManager-gnome-ubuntu/compare_versions.sh
+++ NetworkManager-gnome-ubuntu/compare_versions.sh
@@ -1,23 +1,10 @@
 #!/usr/bin/env bash
 
-SPEC_VER="$(rpmspec -q --qf '%{version}\n' NetworkManager-gnome-ubuntu-Fedora_17.spec | head -1)"
-UBUNTU_VER="$(sed -n 's/^%define[ \t]*_ubuntu_ver[ \t]*\(.*\)$/\1/p' NetworkManager-gnome-ubuntu-Fedora_17.spec)"
-UBUNTU_REL="$(sed -n 's/^%define[ \t]*_ubuntu_rel[ \t]*\(.*\)$/\1/p' NetworkManager-gnome-ubuntu-Fedora_17.spec)"
-SPEC_FCREL="$(sed -n 's/^%define[ \t]*_fedora_rel[ \t]*\(.*\)$/\1/p' NetworkManager-gnome-ubuntu-Fedora_17.spec)"
-GIT_DATE="$(sed -n 's/^%define[ \t]*_git_date[ \t]*\(.*\)$/\1/p' NetworkManager-gnome-ubuntu-Fedora_17.spec)"
+SPECFILE=NetworkManager-gnome-ubuntu-Fedora_17.spec
 
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/network-manager-applet' -O - | sed -n 's/.*>network-manager-applet_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+source "$(dirname ${0})/../version_checker.sh"
 
-echo "Getting latest Fedora version..."
-FEDORA_VER="$(wget -q 'http://pkgs.fedoraproject.org/gitweb/?p=NetworkManager.git;a=blob_plain;f=NetworkManager.spec;hb=f17' -O tmp.spec && rpm -q --qf '%{version} %{release}\n' --specfile tmp.spec | sed 's/\.fc[0-9]*//' | head -n 1 && rm tmp.spec)"
-
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/network-manager-applet/0.9/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
-
-echo ""
-
-echo -e "Fedora version:    ${FEDORA_VER}"
-echo -e "spec file version: ${SPEC_VER} Fedora ${SPEC_FCREL}.git${GIT_DATE} Ubuntu ${UBUNTU_VER} ${UBUNTU_REL}"
-echo -e "Upstream version:  ${UPSTREAM_VER}"
-echo -e "Ubuntu version:    ${UBUNTU_VER[@]}"
+echo -e "spec file version: $(get_spec_version) Fedora $(get_spec_define _fedora_rel).git$(get_spec_define _git_date) Ubuntu $(get_spec_define _ubuntu_ver) $(get_spec_define _ubuntu_rel)"
+echo -e "Fedora version:    $(get_fedora_version NetworkManager 17)"
+echo -e "Upstream version:  $(get_gnome_version network-manager-applet 0.9)"
+echo -e "Ubuntu version:    $(get_ubuntu_version network-manager-applet ${1:-quantal})"
