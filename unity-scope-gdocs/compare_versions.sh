1967d7f0fa7e94e22a62ea518528dc222b15ae71
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-02-01 13:30:13 -0500

    [unity-scope-gdocs] Use new version comparison script

--- unity-scope-gdocs/compare_versions.sh
+++ unity-scope-gdocs/compare_versions.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
-SPEC_VER="$(rpmspec -q --qf '%{version}\n' unity-scope-gdocs.spec | head -1)"
+source "$(dirname ${0})/../version_checker.sh"
 
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/unity-scope-gdocs' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
-
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'https://launchpad.net/unity-lens-gdocs/+download' -O - | sed -n 's/.*unity-scope-gdocs-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
-
-echo ""
-
-echo -e "spec file version: ${SPEC_VER} ${UBUNTU_REL}"
-echo -e "Upstream version:  ${UPSTREAM_VER}"
-echo -e "Ubuntu version:    ${UBUNTU_VER[@]}"
+SPECFILE=unity-scope-gdocs-Fedora_17.spec
+echo -e "F17 spec version: $(get_spec_version)"
+echo -e "Upstream version: $(get_launchpad_version unity-lens-gdocs unity-scope-gdocs)"
+echo -e "Ubuntu version:   $(get_ubuntu_version unity-scope-gdocs ${1:-quantal})"
