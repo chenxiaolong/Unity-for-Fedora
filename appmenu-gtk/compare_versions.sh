f72db7bf114333540903323dc67ea2fa1d433fba
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-05-03 16:06:00 -0400

    [appmenu-gtk] Drop Fedora 17 support

--- appmenu-gtk/compare_versions.sh
+++ appmenu-gtk/compare_versions.sh
@@ -2,8 +2,6 @@
 
 source "$(dirname ${0})/../version_checker.sh"
 
-SPECFILE=appmenu-gtk-Fedora_17.spec
-echo -e "F17 spec version: $(get_spec_version)"
 SPECFILE=appmenu-gtk-Fedora_18.spec
 echo -e "F18 spec version: $(get_spec_version)"
 echo -e "Upstream version: $(get_launchpad_version appmenu-gtk)"
