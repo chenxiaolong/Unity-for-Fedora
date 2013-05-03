8ff28717b32458ddae8b7f845a81281e4a2392d2
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-05-03 17:12:02 -0400

    [libunity-webapps] Drop Fedora 17 support

--- libunity-webapps/compare_versions.sh
+++ libunity-webapps/compare_versions.sh
@@ -2,8 +2,6 @@
 
 source "$(dirname ${0})/../version_checker.sh"
 
-SPECFILE=libunity-webapps-Fedora_17.spec
-echo -e "F17 spec version: $(get_spec_version)"
 SPECFILE=libunity-webapps-Fedora_18.spec
 echo -e "F18 spec version: $(get_spec_version) $(get_spec_release --ubuntu)"
 echo -e "Upstream version: $(get_launchpad_version libunity-webapps unity_webapps)"
