c382b3df26973923ee03ff7c3cb516a0ad8cf50e
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-05-03 15:46:59 -0400

    [appmenu-qt] Removed: Now in Fedora 18 repositories

--- appmenu-qt/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SPECFILE=appmenu-qt.spec
-MULTILIB=true
-MULTILIB_PACKAGES=('appmenu-qt')
-DO_NOT_INSTALL=('appmenu-qt-debuginfo')
-
-source "$(dirname ${0})/../internal_builder.sh"
-build ${1}
--- appmenu-qt/compare_versions.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SPECFILE=appmenu-qt.spec
-
-source "$(dirname ${0})/../version_checker.sh"
-
-echo -e "spec file version: $(get_spec_version)"
-echo -e "Upstream version:  $(get_launchpad_version appmenu-qt)"
-echo -e "Ubuntu version:    $(get_ubuntu_version appmenu-qt ${1:-raring})"
