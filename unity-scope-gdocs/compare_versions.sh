f13d1dc3c9e65d8ae1e21bb237db2fc0ba5eac85
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-05-04 16:22:29 -0400

    [unity-scope-gdocs] Removed: dropping Fedora 17 support

--- unity-scope-gdocs/compare_versions.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(dirname ${0})/../version_checker.sh"
-
-SPECFILE=unity-scope-gdocs-Fedora_17.spec
-echo -e "F17 spec version: $(get_spec_version)"
-echo -e "Upstream version: $(get_launchpad_version unity-lens-gdocs unity-scope-gdocs)"
-echo -e "Ubuntu version:   $(get_ubuntu_version unity-scope-gdocs ${1:-quantal})"
