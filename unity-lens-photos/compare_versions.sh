b4ab3db61ea35006601399c35690c9ddd44e2b5a
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-05-04 15:34:30 -0400

    [unity-lens-photos] Drop Fedora 17 support

--- unity-lens-photos/compare_versions.sh
+++ unity-lens-photos/compare_versions.sh
@@ -2,8 +2,6 @@
 
 source "$(dirname ${0})/../version_checker.sh"
 
-SPECFILE=unity-lens-photos-Fedora_17.spec
-echo -e "F17 spec version: $(get_spec_version)"
 SPECFILE=unity-lens-photos-Fedora_18.spec
 echo -e "F18 spec version: $(get_spec_version)"
 echo -e "Upstream version: $(get_launchpad_version unity-lens-photos)"
