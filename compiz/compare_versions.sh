5e46a6cedb872e2448d5b385aa94252b51d274d4
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-05-04 16:26:07 -0400

    [compiz] Drop Fedora 17 support

--- compiz/compare_versions.sh
+++ compiz/compare_versions.sh
@@ -2,8 +2,6 @@
 
 source "$(dirname ${0})/../version_checker.sh"
 
-SPECFILE=compiz-Fedora_17.spec
-echo -e "F17 spec version: $(get_spec_version)+bzr$(get_spec_define _bzr_rev) $(get_spec_release --ubuntu)"
 SPECFILE=compiz-Fedora_18.spec
 echo -e "F18 spec version: $(get_spec_version) $(get_spec_release --ubuntu)"
 echo -e "Upstream version: $(get_launchpad_version compiz)"
