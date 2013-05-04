2131921269e7d06e86d3aa78ae5cb71acbd65f65
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-05-04 00:10:34 -0400

    [gcc46] Removed: dropping Fedora 17 support

--- gcc46/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SPECFILE=gcc46.spec
-MULTILIB=false
-DO_NOT_INSTALL=('gcc46-debuginfo')
-
-source "$(dirname ${0})/../internal_builder.sh"
-build ${1}
--- gcc46/filter_requires_opensuse.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/usr/lib/rpm/find-requires $* | grep -v '\.so' || true
