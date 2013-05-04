93d741686ef89ac870de1f13d10bb1e20fc91086
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-05-03 20:13:16 -0400

    [gsettings-desktop-schemas-ubuntu] Add Fedora version checking to version checker script

--- gsettings-desktop-schemas-ubuntu/compare_versions.sh
+++ gsettings-desktop-schemas-ubuntu/compare_versions.sh
@@ -5,4 +5,5 @@ source "$(dirname ${0})/../version_checker.sh"
 SPECFILE=gsettings-desktop-schemas-ubuntu-Fedora_18.spec
 echo -e "F18 spec version: $(get_spec_version) $(get_spec_release --ubuntu)"
 echo -e "Upstream version: $(get_gnome_version gsettings-desktop-schemas 3.6)"
+echo -e "Fedora version:   $(get_fedora_version gsettings-desktop-schemas 18)"
 echo -e "Ubuntu version:   $(get_ubuntu_version gsettings-desktop-schemas ${1:-raring})"
