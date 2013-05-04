bc5dcd0cc45999c2e2ae8d347fce3e70d66ef77d
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-05-03 21:26:36 -0400

    [gnome-screensaver-ubuntu] Drop Fedora 17 support

--- gnome-screensaver-ubuntu/compare_versions.sh
+++ gnome-screensaver-ubuntu/compare_versions.sh
@@ -2,8 +2,6 @@
 
 source "$(dirname ${0})/../version_checker.sh"
 
-SPECFILE=gnome-screensaver-ubuntu-Fedora_17.spec
-echo -e "F17 spec version: $(get_spec_version) $(get_spec_release --ubuntu)"
 SPECFILE=gnome-screensaver-ubuntu-Fedora_18.spec
 echo -e "F18 spec version: $(get_spec_version) $(get_spec_release --ubuntu)"
 echo -e "Upstream version: $(get_gnome_version gnome-screensaver 3.6)"
