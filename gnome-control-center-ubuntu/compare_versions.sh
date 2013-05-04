308f50d96d5468687ceb276fb637351b0d34c461
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-05-03 20:44:45 -0400

    [gnome-control-center-ubuntu] Drop Fedora 17 support

--- gnome-control-center-ubuntu/compare_versions.sh
+++ gnome-control-center-ubuntu/compare_versions.sh
@@ -2,9 +2,8 @@
 
 source "$(dirname ${0})/../version_checker.sh"
 
-SPECFILE=gnome-control-center-ubuntu-Fedora_17.spec
-echo -e "F17 spec version: $(get_spec_version) $(get_spec_release --ubuntu)"
 SPECFILE=gnome-control-center-ubuntu-Fedora_18.spec
 echo -e "F18 spec version: $(get_spec_version) $(get_spec_release --ubuntu)"
 echo -e "Upstream version: $(get_gnome_version gnome-control-center 3.6)"
-echo -e "Ubuntu version:   $(get_ubuntu_version gnome-control-center ${1:-raring})"
+echo -e "Fedora version:   $(get_fedora_version control-center 18)"
+echo -e "Ubuntu version:   $(get_ubuntu_version gnome-control-center ${1:-saucy})"
