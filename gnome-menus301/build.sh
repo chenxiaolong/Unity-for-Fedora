a56c8989b7a8ee3512a96dcd35321e0b96bf4811
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-05-04 00:13:58 -0400

    [gnome-menus301] Removed: dropping Fedora 17 support

--- gnome-menus301/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SPECFILE=gnome-menus301.spec
-MULTILIB=true
-MULTILIB_PACKAGES=('gnome-menus301' 'gnome-menus301-devel')
-DO_NOT_INSTALL=('gnome-menus301-debuginfo')
-
-source "$(dirname ${0})/../internal_builder.sh"
-build ${1}
